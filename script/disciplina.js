29ba33c8e169cf1c6112dafc56f5620fc4e051d6
AnthOnnii <anthoni.cardoso@escola.pr.gov.br>
2024-08-30 12:17:42 -0300

    crie a funçlão

--- script/disciplina.js
+++ script/disciplina.js
@@ -1,28 +1,44 @@
-var meuFormulario = document.getElementById("cadastro_disciplina");
-meuFormulario.addEventListener("submit", function(event) {
-  event.preventDefault();
-  cadastrar_turma();
-});
+function salvar() {
+  const disciplina = document.getElementById("nome_disciplina");
+ 
 
-function capturarNome() {
-  const primeiro_numero_inteiro = Number(document.getElementById("nome").value);
-  return primeiro_numero_inteiro;
-}
+  var headers = new Headers();    
+  headers.append("Content-Type", "application/json");
+  headers.append("Access-Control-Allow-Origin", "http://127.0.0.1:5500");
 
-function capturarCargaHoraria() {
- const segundo_numero_inteiro = Number(document.getElementById("carga_horaria").value); 
- return segundo_numero_inteiro;
-}
+  fetch("localhost:8080/disciplina/inserir" ,{
 
+    method: "POST",
+    mode: "cors", // Usando 'cors' para permitir a requisição de origem cruzada
+    cache: "no-cache",
+   
+    // Convertendo o objeto JavaScript para JSON
+    // Esta parte é importante onde você deve passar os parametros (dados) da sua tela
+    body: JSON.stringify({ nome: disciplina }),
 
-function somar() {
-  let primeiro_numero_inteiro = capturarPrimeiroNumeroInteiro();
-  let segundo_numero_inteiro = capturarSegundoNumeroInteiro();
-  let resultado_soma = primeiro_numero_inteiro + segundo_numero_inteiro;
-  imprimirResultadoNaTela(resultado_soma);
-}
+    headers: headers
 
+    //Aqui inicia função then
+  }).then(response => {
 
-function imprimirResultadoNaTela(resultado_soma) {
-  document.getElementById("resultado_soma").value = resultado_soma;
+    if(response.ok) {
+
+      //Esta linha imprime a mensagem no concole
+      console.log("Foi no servidor e voltou");
+
+      //Esta linha carrega a página sucesso
+      window.location.href = "sucesso.html"    
+    } else {
+      //Esta linha imprime a mensagem no console
+      console.log("Aconteceu algo que não foi possivel salvar");
+
+      //Esta linha imprime a mensagem de erro
+      throw new Error("Erro ao tentar salvar");
+    }
+
+  })
+  //Aqui será executado caso a then não seja chamado
+  .catch(error => console.error("Erro!:", error));
+   
+
 }
