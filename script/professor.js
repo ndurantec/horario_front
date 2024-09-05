0535d52fea35f181ef9c0cdcef95fb6b4c795311
Mkelman18 <manuela.denega@escola.pr.gov.br>
2024-09-05 08:16:36 -0300

    tela ajustada

--- script/professor.js
+++ script/professor.js
@@ -1,3 +1,15 @@
   function salvar () {
     console.log('Chegou no javascript');
   }
+
+  function consultar () {
+    console.log('Chegou no javascript');
+  }
+
+  function alterar() {
+    console.log('Chegou no javascript');
+  }
+
+  function apagar () {
+    console.log('Chegou no javascript');
+  }
