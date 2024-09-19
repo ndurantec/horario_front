167662e5b8c64b3de1bb7465fea807dd7e28238f
magalhaesf <fernanda.fatima.magalhaes@escola.pr.gov.br>
2024-09-19 08:45:14 -0300

    criação tela inicial

---
+++ script/home.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const link = document.getElementById('contabt');
+
+    // Define o destino do link dinamicamente
+    link.href = 'conta.html'; // Substitua pelo URL da página desejada
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const link = document.getElementById('fluxobt');
+
+    // Define o destino do link dinamicamente
+    link.href = 'fluxo.html'; // Substitua pelo URL da página desejada
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const link = document.getElementById('operacaobt');
+
+    // Define o destino do link dinamicamente
+    link.href = 'operacao.html'; // Substitua pelo URL da página desejada
+});
