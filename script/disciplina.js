f3205f8d30d12dc10f9ba75bd4a85116d722eeb9
Mkelman18 <manuela.denega@escola.pr.gov.br>
2024-09-19 16:38:33 -0300

    alteração js

--- script/disciplina.js
+++ script/disciplina.js
@@ -193,4 +193,40 @@ function apagar() {
   })
   //Aqui será executado caso a then não seja chamado
   .catch(error => console.error("Erro!:", error));
+
+  function carregarComboOperacao() {
+ 
+    //console.log('Carregou a página e chamou a função');
+  
+    var headers = new Headers();    
+    headers.append("Content-Type", "application/json");
+    headers.append('Access-Control-Allow-Origin', '*');
+  
+    fetch('http://127.0.0.1:8080/operacao/findAll' ,{
+  
+      method: "GET",
+      mode: "cors", // Usando 'cors' para permitir a requisição de origem cruzada
+      cache: "no-cache",
+     
+      // Convertendo o objeto JavaScript para JSON
+      // Esta parte é importante onde você deve passar os parametros (dados) da sua tela
+  
+      headers: headers
+  
+     
+    }).then(response => response.json())
+    .then(data => {
+        const comboBox = document.getElementById('Operacoes');
+        data.forEach(operacao => {
+            const option = document.createElement('option');
+            option.value = operacao.id;
+            option.textContent = operacao.nome;
+            comboBox.appendChild(option);
+        });
+    })
+    .catch(error => console.error('Erro ao carregar locais:', error));
+     
+  
+  }
 }
+
