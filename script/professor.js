77b70b8a7fb2429303152164e273e9c20673f137
Mkelman18 <manuela.denega@escola.pr.gov.br>
2024-09-20 09:35:20 -0300

    Atulizando métodos

--- script/professor.js
+++ script/professor.js
@@ -1,9 +1,10 @@
-  function salvar () {
+document.addEventListener("DOMContentLoaded", function() {
+  localStorage.setItem('id_professor', '');
+});
+
+function salvar () {
     const nome = document.getElementById("nome").value;
     const cpf = document.getElementById("cpf").value;
-    
-    console.log(nome);
-    console.log(cpf);
 
     var headers = new Headers();    
     headers.append("Content-Type", "application/json");
@@ -25,28 +26,34 @@
       headers: headers
   
       //Aqui inicia função then
-    }).then(response => {
-  
-      if(response.ok) {
-  
-        //Esta linha imprime a mensagem no concole
-        console.log("Foi no servidor e voltou");
-  
-        //Esta linha carrega a página sucesso
-        window.location.href = "sucesso.html"    
-      } else {
-        //Esta linha imprime a mensagem no console
-        console.log("Aconteceu algo que não foi possivel salvar");
+     //Aqui inicia função then
+  }).then(response => {
+    if(response.ok) {
+      return response.json(); //transforma a resposta em JSON
+    } else {
+      //Esta linha imprime a mensagem no console
+      console.log('Aconteceu algo que não foi possivel salvar');
+      //Esta linha imprime a mensagem de erro
+      throw new Error('Erro ao tentar salvar');
+    }
 
-        console.log(body);
-  
-        //Esta linha imprime a mensagem de erro
-        throw new Error("Erro ao tentar salvar");
-      }
-  
-    })
-    //Aqui será executado caso a then não seja chamado
-    .catch(error => console.error("Erro!:", error));
+  })
+  .then(data => {
+    //aqui você pode acessar o 'id' retornado do back end
+    const id_professor = data.id;
+    console.log('ID do registro salvo:', id_professor);
+
+    //se quiser armazenar o ID no localStorage
+    localStorage.setItem('id_professor', id_professor);
+
+      console.log('Foi no servidor e voltou');
+
+      //Esta linha carrega a página sucesso
+      //window.location.href = 'sucesso.html'    
+
+  })
+  //Aqui será executado caso a then não seja chamado
+  .catch(error => console.error('Erro!:', error));
 }
 
   function consultar () {
@@ -100,13 +107,19 @@
     const nome = document.getElementById("nome").value;
     const cpf = document.getElementById("cpf").value;
  
+    const id_professor = localStorage.getItem('id_professor');
+    console.log(id_professor);
 
     var headers = new Headers();    
     headers.append("Content-Type", "application/json");
     headers.append("Access-Control-Allow-Origin", "*");
   
-    fetch("http://127.0.0.1:8080/professor/update" ,{
-  
+    console.log(`http://127.0.0.1:8080/professor/${id_professor}`);
+
+    fetch(`http://127.0.0.1:8080/professor/${id_professor}` ,{
+      
+      
+
       method: "PUT",
       mode: "cors", // Usando 'cors' para permitir a requisição de origem cruzada
       cache: "no-cache",
