798564bd3860bfa2c2da02037520ffcd437f504d
mltomacheski <tomacheski.lara@escola.pr.gov.br>
2024-11-04 08:26:10 -0300

    Alterei oa validação do campo nome professor para que limpe caso o nome não seja vállido

--- script/professor.js
+++ script/professor.js
@@ -7,18 +7,22 @@ function salvar () {
 
     if (nome.trim() === '' || nome.length < 2) {
       alert("Nome Inválido! Deve ter pelo menos 2 caracteres e conter apenas letras.");
+      document.getElementById('nome').value = '';
+      document.getElementById('nome').focus();
       return false;
   }
 
   const regex = /^[A-Za-zÀ-ÿ\s]+$/;
   if (!regex.test(nome)) {
       alert("Nome Inválido! Deve conter apenas letras.");
+      document.getElementById('nome').value = '';
+      document.getElementById('nome').focus();
       return false;
   }
+  
 
   alert("Nome Válido!");
   return true;
-}
 
 // Função para validar o CPF
 function validarCPF(cpf) {
@@ -32,7 +36,10 @@ function validarCPF(cpf) {
 
   alert("CPF Válido!");
   return true;
-}
+
+}}
+
+
 
       // Verifica se o nome não está vazio e tem pelo menos 2 caracteres
     /*  if (nome.trim() === '' || nome.length < 2) {
