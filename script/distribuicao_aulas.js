79f5d8a598436ed918ebb1f9e7ed0f465ca05c64
Mkelman18 <manuela.denega@escola.pr.gov.br>
2024-11-08 10:38:30 -0300

    alerações do terceiro ano

--- script/distribuicao_aulas.js
+++ script/distribuicao_aulas.js
@@ -1,8 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
     carregarComboProfessor();
+    capturarIdDoProfessor();
   });
 
-function carregarComboProfessor() {
+  function carregarComboProfessor() {
  
     //console.log('Carregou a página e chamou a função');
   
@@ -32,7 +33,68 @@ function carregarComboProfessor() {
             comboBox.appendChild(option);
         });
     })
-    .catch(error => console.error('Erro ao carregar locais:', error));
+    .catch(error => console.error('Erro ao carregar locais:', error));  
+  }
+
+
+  function capturarIdDoProfessor() {
+    const selectElement = document.getElementById('professor');
+    
+    // Adiciona o evento change
+    selectElement.addEventListener('change', function() {
+      // Captura o valor selecionado
+      const valorSelecionado = selectElement.value;
+      
+      // Exibe o valor selecionado
+      document.getElementById('resultado').innerText = `Você selecionou: ${valorSelecionado}`;
+
+      consultarDisciplinaDoProfessor(valorSelecionado);
+
+    });
+
+  }
+
+  function consultarDisciplinaDoProfessor(valorSelecionado) {                                            
      
+    var headers = new Headers();    
+    headers.append("Content-Type", "application/json");
+    headers.append("Access-Control-Allow-Origin", "*");
+    
+    fetch(`http://127.0.0.1:8080/professor/${valorSelecionado}` ,{
   
+      method: "GET",
+  
+  
+      //mode: "cors", // Usando 'cors' para permitir a requisição de origem cruzada
+      //cache: "no-cache",
+     
+      // Convertendo o objeto JavaScript para JSON
+      // Esta parte é importante onde você deve passar os parametros (dados) da sua tela
+      //body: JSON.stringify({ 
+          //nome: nome,
+          //professor: {id: professor}
+      // }),
+  
+      headers: {
+        'Content-Type' : 'application/json'
+      }
+      //Aqui inicia função then
+    }).then(response => {
+  
+      if (!response.ok) {
+        throw new Error('Erro ao buscar professores')
+      } 
+      return response.json();
+    })
+    .then(id_disciplina => {
+      console.log("ID da conta recebida:", id_disciplina); // Aqui o id é diretamente o retorno
+  
+        localStorage.setItem('id_professor', JSON.stringify(id_disciplina));
+  
+        console.log('Id dos professores foram salvos no localStorage')
+        
+    })
+    .catch(error => {
+      console.error("Erro capturado no catch:", error);
+    });
   }
